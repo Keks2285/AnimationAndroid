c9404d176681a67427d8f87e59df60dde36b9338
Илья <ilion23082003@gmail.com>
2022-06-09 19:09:53 +0300

    first

---
+++ RotateAnimationAndroid/app/src/main/java/com/example/rotateanimationandroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.rotateanimationandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnOrientation;
+    ImageView myImageView;
+    Button buttonRotateCenter;
+    Button buttonRotateCorner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myImageView = findViewById(R.id.imageView);
+        buttonRotateCenter = findViewById(R.id.rotatecenter);
+        buttonRotateCorner = findViewById(R.id.rotatecorner);
+
+        final Animation animationRotateCenter = AnimationUtils.loadAnimation(
+                this, R.anim.rotate_center);
+        final Animation animationRotateCorner = AnimationUtils.loadAnimation(
+                this, R.anim.rotate_corner);
+
+        buttonRotateCenter.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myImageView.startAnimation(animationRotateCenter);
+            }
+        });
+
+        buttonRotateCorner.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myImageView.startAnimation(animationRotateCorner);
+            }
+        });
+    }
+}
